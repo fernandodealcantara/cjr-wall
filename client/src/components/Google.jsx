4bed615539f3c6ab661e22b738f944f06f6b3e87
Cassio-hue <cassiocvtb2003@gmail.com>
2022-08-23 23:08:21 -0300

    ✨ Botão de login do google

---
+++ client/src/components/Google.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+import jwtDecode from 'jwt-decode'
+
+function handleCallbackResponse(response) {
+    console.log(`Encoded JWT ID token:  ${response.credential}`)
+    const userObject = jwtDecode(response.credential)
+    console.log(userObject)
+}
+
+export default function GoogleButton() {
+
+    useEffect(() => {
+        /* global google */
+        google.accounts.id.initialize({
+            client_id: import.meta.env.VITE_CLIENT_ID,
+            callback: handleCallbackResponse
+        })
+    
+        google.accounts.id.renderButton(
+            document.getElementById("signInDiv"),
+            {theme: "outline", size: "small"}
+        )
+    }, [])
+    return (
+        <div id="signInDiv"/>
+    )
+}
