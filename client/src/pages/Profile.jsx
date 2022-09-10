dc451d987f7d647eeb2f024fd2a9cd478a1abca7
Cassio-hue <cassiocvtb2003@gmail.com>
2022-09-09 23:38:42 -0300

    Pagina de perfil

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+function Profile() {
+    return (
+      <>
+        <h1>Profile</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat saepe tempore quis at itaque error maxime, a, laborum exercitationem neque nobis assumenda natus dolore laudantium tempora accusamus dicta! A, distinctio.</p>
+      </>
+    )
+  }
+  
+  export default Profile
+  
