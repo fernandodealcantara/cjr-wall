676ba7ca8381d20f4d9b999bd48df3517a236a86
Cassio-hue <cassiocvtb2003@gmail.com>
2022-09-09 23:39:39 -0300

    Template do Tijolo

---
+++ client/src/components/TijoloNucleo.jsx
@@ -0,0 +1,20 @@
+// const icone = 'https://avatars.githubusercontent.com/u/62450919'
+
+export default function TijoloNUC({color, avatar, userName}) {
+  return (
+    <div
+      className={
+        `w-[18rem] h-[6.5rem] rounded-lg border-4 border-[#000000] bg-center bg-no-repeat bg-cover flex items-center gap-x-[5rem] px-[1rem] bg-origin-border bg-${color}`
+      }
+    >
+      <img
+        className="border-4 border-[#000000] rounded-full w-[4rem] h-[4rem]"
+        src={avatar}
+        alt="avatar-user"
+      />
+      <span className="font-bold bg-[#000000] px-[1rem] rounded-lg ">
+        {userName}
+      </span>
+    </div>
+  )
+}
