aa2ca7a3f8811899d9a4810b602925a928b16e7d
Cassio-hue <cassiocvtb2003@gmail.com>
2022-10-08 17:04:55 -0300

    feat: criando o profile e instalando o slate

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,16 @@
+import { Custom } from '../components/Editable'
+
 function Profile() {
   return (
-    <div className="grid place-items-center h-screen">
-      <div className="bg-NIPCard grid overflow-hidden grid-cols-3 grid-rows-2 gap-1 h-auto">
-        <div className="bg-NIP box m-[10vh] rounded-lg">1</div>
-        <div className="bg-NIP box col-start-1 m-[8%] rounded-lg">2</div>
-        <div className="bg-NIP box row-start-1 row-end-3 col-start-2 m-[8%] rounded-lg">3</div>
+    <>
+      <div className="p-[20px] bg-NIP grid grid-cols-1 sm:grid-cols-3 gap-x-4 gap-y-4 grid-flow-row-dense">
+        <div className="bg-NIPCard rounded-lg shadow-xl min-h-[10vh]" />
+        <div className="p-[20px] bg-NIPCard rounded-lg shadow-xl min-h-[10vh] row-span-2 col-span-2">
+          <Custom />
+        </div>
+        <div className="bg-NIPCard rounded-lg shadow-xl min-h-[25vh]" />
       </div>
-    </div>
+    </>
   )
 }
 
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -3,31 +3,34 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     colors: {
-      'NAV': '#6A1612',
-      'NAVCard': '#851610',
-      'NUT': '#1D3557',
-      'NUTCard': '#457B9D',
-      'NOE': '#0B5F2F',
-      'NOECard': '#0D9347',
-      'NIP': '#6C2144',
-      'NIPCard': '#AE326C',
-      'NDP': '#39484F',
-      'NDPCard': '#607D8B',
+      NAV: '#6A1612',
+      NAVCard: '#851610',
+      NUT: '#1D3557',
+      NUTCard: '#457B9D',
+      NOE: '#0B5F2F',
+      NOECard: '#0D9347',
+      NIP: '#6C2144',
+      NIPCard: '#AE326C',
+      NDP: '#39484F',
+      NDPCard: '#607D8B',
     },
     extend: {
       backgroundImage: {
-        'tijNAV':
+        tijNAV:
           "url('/src/assets/grid.svg'), linear-gradient(180deg, #DC4A3C 0%, rgba(220, 74, 60, 0) 100%)",
-        'tijNUT':
+        tijNUT:
           "url('/src/assets/grid.svg'), linear-gradient(180deg, #119CDC 0%, rgba(17, 156, 220, 0) 100%)",
-        'tijNOE':
+        tijNOE:
           "url('/src/assets/grid.svg'), linear-gradient(180deg, #2EC26E 0%, rgba(46, 194, 110, 0) 100%)",
-        'tijNIP':
+        tijNIP:
           "url('/src/assets/grid.svg'), linear-gradient(180deg, #CC4784 0%, rgba(204, 71, 132, 0) 100%)",
-        'tijNDP':
+        tijNDP:
           "url('/src/assets/grid.svg'), linear-gradient(180deg, #607D8B 0%, rgba(96, 125, 139, 0) 100%)",
       },
       fontFamily: {
