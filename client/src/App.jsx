420b2b6b7b93ac683b6edad2b85236743fdcc2f1
Fernando de Alcantara <45339608+fernandodealcantara@users.noreply.github.com>
2022-10-10 11:26:19 -0300

    removes authprovider and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
-import { AuthProvider } from './contexts/AuthContext'
-import { RequireAuth } from './components/RequireAuth'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import Layout from './components/Layout'
-import BrickProfile from './pages/BrickProfile'
 
 function App() {
   return (
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="profile">
-            <Route
-              index
-              element={
-                <RequireAuth>
-                  <Profile />
-                </RequireAuth>
-              }
-            />
-            <Route path=":userId" element={<BrickProfile />} />
-          </Route>
-          <Route path="*" element={<h1>Nada por aqui 😜</h1>} />
-        </Route>
-      </Routes>
-    </AuthProvider>
+    <Routes>
+      <Route path="/" element={<h1>Hello </h1>} />
+    </Routes>
   )
 }
 
